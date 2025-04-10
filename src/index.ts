cfdb9dd2576122c7c94cdbb640a3dcd4ad21e5a0
akashicxing <akashicxing@gmail.com>
2025-04-10 16:35:16 +0800

    Initial commit: AI Tools Box project setup

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { tools as tools1 } from './data1';
+import { tools as tools2 } from './data2';
+import { categories } from './categories';
+
+// 合并所有工具数据
+export const tools = [...tools1, ...tools2];
+
+export { categories }; 
